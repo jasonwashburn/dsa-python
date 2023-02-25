362d4b7c2027bb8b44624cc80136103861d5b07f
Jason Washburn <jason@shyftsolutions.io>
2023-02-24 18:24:34 -0600

    adds enqueue to queue

--- src/dsa/queue.py
+++ src/dsa/queue.py
@@ -12,8 +12,8 @@ class Node:
         Args:
             value (Any): The value to store in the node.
         """
-        self.value = value
-        self.next = None
+        self.value: Any = value
+        self.next: Node | None = None
 
 
 class Queue:
@@ -25,9 +25,9 @@ class Queue:
         Args:
             value (Any): The value to store in the queue.
         """
-        self.first = Node(value)
-        self.last = self.first
-        self.length = 1
+        self.first: Node | None = Node(value)
+        self.last: Node | None = self.first
+        self.length: int = 1
 
     def __len__(self) -> int:
         """Return the length of the queue."""
@@ -39,3 +39,21 @@ class Queue:
         while temp is not None:
             print(temp.value)  # noqa: T201
             temp = temp.next
+
+    def enqueue(self, value: Any) -> bool:
+        """Add a node to the queue.
+
+        Args:
+            value (Any): The value to store in the node.
+
+        Returns:
+            bool: True if the node was added, False otherwise.
+        """
+        new_node = Node(value)
+        if self.length == 0 or self.first is None or self.last is None:
+            self.first = new_node
+            self.last = new_node
+        self.last.next = new_node
+        self.last = new_node
+        self.length += 1
+        return True
--- tests/dsa/test_queue.py
+++ tests/dsa/test_queue.py
@@ -3,7 +3,25 @@
 from io import StringIO
 from unittest.mock import patch
 
+import pytest
+
 from dsa.queue import Node, Queue
+
+
+@pytest.fixture()
+def empty_queue() -> Queue:
+    """Create an empty queue."""
+    queue = Queue(value=4)
+    queue.first = None
+    queue.last = None
+    queue.length = 0
+    return queue
+
+
+@pytest.fixture()
+def single_item_queue() -> Queue:
+    """Create a queue with a single item."""
+    return Queue(value=4)
 
 
 def test_node_builds() -> None:
@@ -21,15 +39,31 @@ def test_queue_builds() -> None:
     assert victim.length == 1
 
 
-def test_queue_supports_len() -> None:
+def test_queue_supports_len(single_item_queue) -> None:
     """Test that a queue can be built."""
-    victim = Queue(value=4)
-    assert len(victim) == 1
+    assert len(single_item_queue) == 1
 
 
 @patch("sys.stdout", new_callable=StringIO)
-def test_print_queue(mock_stdout) -> None:
+def test_print_queue(mock_stdout, single_item_queue) -> None:
     """Test that print_list() prints the list to stdout."""
-    victim = Queue(value=4)
-    victim.print_queue()
+    single_item_queue.print_queue()
     assert mock_stdout.getvalue() == "4\n"
+
+
+def test_enqueue(single_item_queue) -> None:
+    """Test that enqueue() adds a node to the queue."""
+    victim = single_item_queue
+    victim.enqueue(5)
+    assert victim.length == 2
+    assert victim.first.value == 4
+    assert victim.last.value == 5
+
+
+def test_enqueue_on_empty_queue(empty_queue) -> None:
+    """Test that enqueue() adds a node to the queue."""
+    victim = empty_queue
+    victim.enqueue(5)
+    assert victim.length == 1
+    assert victim.first.value == 5
+    assert victim.last.value == 5
