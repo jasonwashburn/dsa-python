028a8a50a2cc3897cf1373eddb7dcf446c268965
Jason Washburn <jason.washburn@gmail.com>
2023-03-01 06:53:23 -0600

    adds hash method to hashtable

--- src/dsa/hashtable.py
+++ src/dsa/hashtable.py
@@ -11,3 +11,9 @@ class HashTable:
             size (int, optional): The size of the HashTable. Defaults to 7.
         """
         self.data_map: list = [None] * size
+
+    def _hash(self, key: str) -> int:
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
--- tests/dsa/test_hashtable.py
+++ tests/dsa/test_hashtable.py
@@ -1,4 +1,6 @@
 """Tests for HashTable class."""
+import pytest
+
 from dsa.hashtable import HashTable
 
 
@@ -12,3 +14,28 @@ def test_hashtable_builds_with_size() -> None:
     """Tests that the HashTable class builds with a size."""
     victim = HashTable(11)
     assert len(victim.data_map) == 11
+
+
+@pytest.mark.parametrize(
+    ("size", "key", "expected"),
+    [
+        (7, "abc", 0),
+        (7, "bcd", 6),
+        (7, "cde", 5),
+        (7, "def", 4),
+        (9, "abc", 3),
+        (9, "bcd", 0),
+        (9, "cde", 6),
+        (9, "def", 3),
+    ],
+)
+def test_hash(size, key, expected) -> None:
+    """Tests that the HashTable class builds with a size.
+
+    Args:
+        size (int): The size of the HashTable.
+        key (str): The key to be hashed.
+        expected (int): The expected hash.
+    """
+    victim = HashTable(size=size)
+    assert victim._hash(key) == expected
