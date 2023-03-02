c63669aa631372c1dcea7f30172cdddc39589c3c
Jason Washburn <jason.washburn@gmail.com>
2023-03-01 18:04:12 -0600

    adds set_value to hashtable

--- src/dsa/hashtable.py
+++ src/dsa/hashtable.py
@@ -17,3 +17,16 @@ class HashTable:
         for letter in key:
             my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
         return my_hash
+
+    def set_value(self, key: str, value: object) -> None:
+        """Sets a value in the HashTable.
+
+        Args:
+            key (str): The key to be hashed.
+            value (object): The value to be stored.
+        """
+        index = self._hash(key)
+        if self.data_map[index]:
+            self.data_map[index].append((key, value))
+        else:
+            self.data_map[index] = [(key, value)]
--- tests/dsa/test_hashtable.py
+++ tests/dsa/test_hashtable.py
@@ -1,4 +1,5 @@
 """Tests for HashTable class."""
+
 import pytest
 
 from dsa.hashtable import HashTable
@@ -39,3 +40,62 @@ def test_hash(size, key, expected) -> None:
     """
     victim = HashTable(size=size)
     assert victim._hash(key) == expected
+
+
+@pytest.mark.parametrize(
+    ("key", "value", "expected_index", "expected_value"),
+    [
+        ("abc", 123, 0, [("abc", 123)]),
+        ("bcd", 456, 6, [("bcd", 456)]),
+        ("cde", 789, 5, [("cde", 789)]),
+    ],
+)
+def test_set(
+    key: str,
+    value: object,
+    expected_index: int,
+    expected_value: list[tuple[str, object]],
+) -> None:
+    """Tests that the HashTable sets correctly.
+
+    Args:
+        key (str): The key to be hashed.
+        value (object): The value to be stored.
+        expected_index (int): The expected index.
+        expected_value (list[tuple[str, object]]): The expected value.
+    """
+    victim = HashTable()
+    victim.set_value(key=key, value=value)
+    assert victim.data_map[expected_index] == expected_value
+
+
+@pytest.mark.parametrize(
+    (
+        "key_value_one",
+        "key_value_two",
+        "expected_index",
+        "expected_value",
+    ),
+    [
+        (("abc", 123), ("cba", 321), 0, [("abc", 123), ("cba", 321)]),
+        (("bcd", 456), ("dcb", 654), 6, [("bcd", 456), ("dcb", 654)]),
+    ],
+)
+def test_set_with_collision(
+    key_value_one: tuple[str, object],
+    key_value_two: tuple[str, object],
+    expected_index: int,
+    expected_value: list[tuple[str, object]],
+) -> None:
+    """Tests that the HashTable sets correctly with collisions.
+
+    Args:
+        key_value_one (tuple[str, object]): A key value pair.
+        key_value_two (tuple[str, object]): A key value pair.
+        expected_index (int): The expected index.
+        expected_value (list[tuple[str, object]]): The expected value.
+    """
+    victim = HashTable()
+    victim.set_value(key=key_value_one[0], value=key_value_one[1])
+    victim.set_value(key=key_value_two[0], value=key_value_two[1])
+    assert victim.data_map[expected_index] == expected_value
